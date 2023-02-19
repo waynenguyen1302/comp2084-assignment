eb2328f29153d4be305ea43eba196ec2d371a85a
waynenguyen1302 <111101794+waynenguyen1302@users.noreply.github.com>
2023-02-19 14:16:45 -0800

    Added some authentication functionalities

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    // add Identity Role
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
